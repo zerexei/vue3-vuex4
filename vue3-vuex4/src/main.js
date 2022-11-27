8df88f51c9eb70ee5525fd9f2897e6065bda1b7a
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-11-27 19:51:10 +0800

    Todo app w/ vuex + localStorage

--- vue3-vuex4/src/main.js
+++ vue3-vuex4/src/main.js
@@ -1,11 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import router from "./router";
+import store from "./store";
+import App from "./App.vue";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
+app.use(store);
 
-app.mount('#app')
+app.mount("#app");
