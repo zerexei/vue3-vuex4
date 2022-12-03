73b09849ce681036a4c8d39f311ad13d13046e3f
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-12-03 20:02:07 +0800

    Add watch w/ reactive object getter

--- vue-day1/vue3-composition/src/views/HomeView.vue
+++ vue-day1/vue3-composition/src/views/HomeView.vue
@@ -1,5 +1,20 @@
 <script setup>
+import { reactive, watch } from 'vue'
+
 import TheWelcome from '../components/TheWelcome.vue'
+
+const obj = reactive({
+  label: "My Counter",
+  counter: 0,
+});
+
+// () => obj.counter: getter
+watch(() => obj.counter, (newObjCounterValue) => {
+  console.log(newObjCounterValue);
+});
+watch()
+
+
 </script>
 
 <template>
